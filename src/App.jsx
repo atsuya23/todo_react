5d5f9e4d4109a6a26b09e580655af69e9ade8e3b
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-23 18:20:39 +0900

    削除の実装

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...undoneTodos];
+    newTodos.splice(index, 1);
+    setUndoneTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -28,12 +34,12 @@ export const App = () => {
       <div className="done">
         <p className="title">未完了のTODO</p>
         <ul>
-          {undoneTodos.map((todo) => {
+          {undoneTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
